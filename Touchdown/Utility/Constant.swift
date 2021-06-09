8e7f8aff4df9a5099c3e9cb9845621b0453a1c53
ogi-98 <oguzkaya99@hotmail.com>
2021-06-09 20:00:47 +0300

    Footer and folders

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Oğuz Kaya on 9.06.2021.
+//
+
+import SwiftUI
+
+//DATA
+
+//COLOR
+
+let colorBackground:Color = Color("ColorBackground")
+let colorGray:Color = Color(UIColor.systemGray4)
+
+//LAYOUT
+//UX
+//API
+//IMAGE
+//FONT
+//STRING
+//MISC
