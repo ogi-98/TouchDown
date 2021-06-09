0f63ec91d94d8977338e1bccc3a5c3aafb73c89d
ogi-98 <oguzkaya99@hotmail.com>
2021-06-09 19:18:13 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Oğuz Kaya on 9.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
